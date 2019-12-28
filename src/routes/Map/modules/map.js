c346917619b50d24ff73d64c9c8ac50347da72f6
Abhishar Jangir <abhishar.jangir@idc.bedbath.com>
2019-12-28 20:29:53 +0530

    http - https

--- src/routes/Map/modules/map.js
+++ src/routes/Map/modules/map.js
@@ -28,64 +28,60 @@ export const LOADING_STOP = 'LOADING_STOP'
 // ------------------------------------
 // Actions
 // ------------------------------------
-export function initISSPosition() {
+export function initISSPosition () {
   return function (dispatch) {
-    dispatch({type : 'LOADING_START'})
-    return fetch(`http://api.open-notify.org/iss-now.json`).then((response) => {
+    dispatch({ type : 'LOADING_START' })
+    return fetch(`https://api.open-notify.org/iss-now.json`).then((response) => {
       response.json().then(json => {
-
-        if(json.message === 'success') {
+        if (json.message === 'success') {
           dispatch({ type: MAP_ADD_MARKER, payload: [parseFloat(json.iss_position.latitude), parseFloat(json.iss_position.longitude)] })
           dispatch({ type: 'ADD_POINT_TO_LINE', payload: [parseFloat(json.iss_position.longitude), parseFloat(json.iss_position.latitude)] })
-          dispatch({type : 'LOADING_STOP'})
+          dispatch({ type : 'LOADING_STOP' })
         } else console.log(json)
       })
     }).catch(error => {
-      throw(error);
+      throw (error)
     })
   }
 }
 
-export function whoIsInSpace() {
+export function whoIsInSpace () {
   return function (dispatch) {
-    return fetch(`http://api.open-notify.org/astros.json`).then((response) => {
+    return fetch(`https://api.open-notify.org/astros.json`).then((response) => {
       response.json().then(json => {
-        if(json.message === 'success')
-          dispatch({ type: UPDATE_PEOPLE_IN_SPACE, payload: json.people })
-        else console.log(json)
+        if (json.message === 'success') { dispatch({ type: UPDATE_PEOPLE_IN_SPACE, payload: json.people }) } else console.log(json)
       })
     }).catch(error => {
-      throw(error);
+      throw (error)
     })
   }
 }
 
-
-export function whenWillPassThroughYou(c) {
+export function whenWillPassThroughYou (c) {
   return function (dispatch) {
-    return fetch(`http://api.open-notify.org/iss-pass.json?lat=${c.lat}&lon=${c.long}`, { mode: 'cors' })
+    return fetch(`https://api.open-notify.org/iss-pass.json?lat=${c.lat}&lon=${c.long}`, { mode: 'cors' })
       .then((response) => {
         response.json().then(json => {
           dispatch({ type: SAVE_WHEN_WILL_PASS_THROUGH, payload: json })
         })
       }).catch(error => {
-        throw(error);
+        throw (error)
       })
   }
 }
 
-export function togglePeopleInfo() {
+export function togglePeopleInfo () {
   return {
     type: TOGGLE_PEOPLE_INFO
   }
 }
-export function togglePassInfo() {
+export function togglePassInfo () {
   return {
     type: TOGGLE_PASS_INFO
   }
 }
 
-export function toggleBasemap(basemap) {
+export function toggleBasemap (basemap) {
   return {
     type: TOGGLE_BASEMAP,
     payload: basemap
@@ -102,9 +98,9 @@ const ACTION_HANDLERS = {
   [TOGGLE_PASS_INFO]: (state, action) => Object.assign({}, state, { info: { pass: !state.info.pass, people: state.info.people } }),
   [TOGGLE_PEOPLE_INFO]: (state, action) => Object.assign({}, state, { info: { people: !state.info.people, pass: state.info.pass } }),
   [TOGGLE_BASEMAP]: (state, action) => Object.assign({}, state, { basemap: action.payload }),
-  [ADD_POINT_TO_LINE]: (state, action) => Object.assign({}, state, { line: state.line.concat([action.payload])}),
-  [LOADING_START] : (state,action) => Object.assign({}, state, { loading: true }),
-  [LOADING_STOP] : (state,action) => Object.assign({}, state, { loading: false })
+  [ADD_POINT_TO_LINE]: (state, action) => Object.assign({}, state, { line: state.line.concat([action.payload]) }),
+  [LOADING_START] : (state, action) => Object.assign({}, state, { loading: true }),
+  [LOADING_STOP] : (state, action) => Object.assign({}, state, { loading: false })
 }
 
 // ------------------------------------
@@ -119,11 +115,11 @@ const initialState = {
   },
   basemap: 'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
   line: [
-    [0,0]
+    [0, 0]
   ]
 }
 
-export default function counterReducer(state = initialState, action) {
+export default function counterReducer (state = initialState, action) {
   const handler = ACTION_HANDLERS[action.type]
   return handler ? handler(state, action) : state
 }
